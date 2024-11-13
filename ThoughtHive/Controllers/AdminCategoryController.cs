9ba8b33237b8b2a029ee4b0057ab2cbaec265983
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-11-13 17:52:55 +0300

    Update

---
+++ ThoughtHive/Controllers/AdminCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThoughtHive.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cM = new CategoryManager(new EFCategoryDal());
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = cM.GetList();
+            return View(values);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult results = validator.Validate(category);
+            if (results.IsValid)
+            {
+                cM.CategoryAddBL(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
