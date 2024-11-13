b97077e88a83288b60562a0785251808feecfe7a
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-11-13 15:41:33 +0300

    Changes were made to the layers. Admin theme added.

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,24 +10,24 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager:ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category> ();
+        ICategoryDal _categoryDal;
 
-        public List<Category> GetAllBL()
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            return repo.List();
+            _categoryDal = categoryDal;
         }
 
         public void CategoryAddBL(Category category)
         {
-            if(category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDesription == "" || category.CategoryName.Length >= 51)
-            {
-                //hata mesajı
-            }
-            else { 
-            repo.Insert(category);
-            }
+            _categoryDal.Insert(category);
         }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+
     }
 }
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
+            RuleFor(x => x.CategoryDesription).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz");
+            RuleFor(x => x.CategoryDesription).MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz");
+            RuleFor(x => x.CategoryDesription).MaximumLength(20).WithMessage("Lütfen en fazla 20 karakter giriniz");
+        }
+    }
+}
